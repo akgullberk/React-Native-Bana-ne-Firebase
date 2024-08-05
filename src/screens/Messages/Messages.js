c19fb9d6e92bb0fd08cc80ae953727f7d3d847e3
Berk Akgül <berkak_gul51@hotmail.com>
2024-08-05 18:09:31 +0300

    sendContent

--- src/screens/Messages/Messages.js
+++ src/screens/Messages/Messages.js
@@ -1,10 +1,23 @@
-import {Text, View } from 'react-native'
-import React, { useState } from 'react'
+import {FlatList, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { styles } from './styles'
 import { FloatingButton, Modal } from '../../components'
+import database from '@react-native-firebase/database'
+import auth from '@react-native-firebase/auth'
 
 const Messages = () => {
   const [modalVisible, setModalVisible] = useState(false)
+  const [contentList, setContentList] = useState([])
+
+  useEffect(() => {
+    database()
+    .ref('messages/')
+    .on("value",snapshot =>{
+      const contentData = snapshot.val();
+      console.log(contentData)
+    });
+  }, [])
+  
 
   const handleToggle = () => {
     setModalVisible(!modalVisible)
@@ -12,11 +25,25 @@ const Messages = () => {
 
   const handleSendContent = (content) => {
     handleToggle()
-    console.log(content)
+    sendContent(content)
+  }
+
+  function sendContent(content) {
+    const userMail = auth().currentUser.email
+      const contentObject = {
+        text: content,
+        username: userMail.split("@")[0],
+        date: new Date().toISOString()
+      };
+
+      database().ref('messages/').push(contentObject)
   }
 
   return (
     <View>
+      <FlatList
+
+      />
       <FloatingButton onPress={handleToggle} />
       <Modal 
         visible={modalVisible} 
